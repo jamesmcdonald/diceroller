8dc077f5576ddb74b034cebfce2c6b890a401fe5
James McDonald <james@jamesmcdonald.com>
2025-03-08 21:19:21 +0100

    Add Watch app skeleton

---
+++ Diceroller Watch Watch App/Diceroller_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Diceroller_WatchApp.swift
+//  Diceroller Watch Watch App
+//
+//  Created by James McDonald on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Diceroller_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Diceroller Watch Watch AppTests/Diceroller_Watch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Diceroller_Watch_Watch_AppTests.swift
+//  Diceroller Watch Watch AppTests
+//
+//  Created by James McDonald on 08/03/2025.
+//
+
+import Testing
+@testable import Diceroller_Watch_Watch_App
+
+struct Diceroller_Watch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
