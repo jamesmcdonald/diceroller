ad5eac3cfe55a12dc8313cc5c702b1421a25ae1e
James McDonald <james@jamesmcdonald.com>
2025-03-08 15:36:32 +0100

    Initial import

---
+++ Diceroller/DicerollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicerollerApp.swift
+//  Diceroller
+//
+//  Created by James McDonald on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DicerollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DicerollerTests/DicerollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DicerollerTests.swift
+//  DicerollerTests
+//
+//  Created by James McDonald on 08/03/2025.
+//
+
+import Testing
+@testable import Diceroller
+
+struct DicerollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
